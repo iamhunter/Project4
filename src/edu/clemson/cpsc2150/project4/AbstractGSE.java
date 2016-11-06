da8632c0fb3a479535a0fecb38dd45f0c18338a9
iams3cond <andr3whunter@gmail.com>
2016-11-06 17:48:48 -0500

    Finished AbstractGSE.  No errors but tons of warnings.. Time to make a driver and then go back and fix all of them

--- src/edu/clemson/cpsc2150/project4/AbstractGSE.java
+++ src/edu/clemson/cpsc2150/project4/AbstractGSE.java
@@ -1,5 +1,8 @@
 package edu.clemson.cpsc2150.project4;
 
+import com.sun.tools.javac.jvm.Gen;
+import sun.net.www.content.text.Generic;
+
 /**
  * Created by andrewmarionhunter on 11/2/16.
  */
@@ -23,8 +26,6 @@ public abstract class AbstractGSE<T> implements GenericSetExtended<T> {
             rhs.insert((T)tempSet.removeAny());
         }
 
-
-
         while(copiedThis.size() > 0)
         {
             T tempOrary = (T)copiedThis.removeAny();
@@ -41,20 +42,97 @@ public abstract class AbstractGSE<T> implements GenericSetExtended<T> {
     }
     @Override
     public final void intersect(GenericSet<T> rhs) {
-/* insert code here */
+        GenericSet copiedThis = new GenericSet2();
+        GenericSet copiedrhs = new GenericSet2();
+        this.copy(copiedThis);
+
+        GenericSet tempSet = new GenericSet2();
+        while(rhs.size() > 0)
+        {
+            T temp = rhs.removeAny();
+            copiedrhs.insert(temp);
+            tempSet.insert(temp);
+        }
+        while(tempSet.size() > 0)
+        {
+            rhs.insert((T)tempSet.removeAny());
+        }
+
+        while(copiedThis.size() > 0)
+        {
+            T tempOrary = (T)copiedThis.removeAny();
+            if(copiedrhs.contains(tempOrary))
+            {
+                this.insert(tempOrary);
+            }
+        }
     }
     @Override
     public final void difference(GenericSet<T> rhs) {
-/* insert code here */
+        GenericSet copiedThis = new GenericSet2();
+        GenericSet copiedrhs = new GenericSet2();
+        this.copy(copiedThis);
+
+        GenericSet tempSet = new GenericSet2();
+        while(rhs.size() > 0)
+        {
+            T temp = rhs.removeAny();
+            copiedrhs.insert(temp);
+            tempSet.insert(temp);
+        }
+        while(tempSet.size() > 0)
+        {
+            rhs.insert((T)tempSet.removeAny());
+        }
+
+        while(copiedThis.size() > 0)
+        {
+            T tempOrary = (T)copiedThis.removeAny();
+            if(!copiedrhs.contains(tempOrary))
+            {
+                this.insert(tempOrary);
+            }
+        }
     }
     @Override
     public final void copy(GenericSet<T> set) {
-/* insert code here */
+        GenericSet tempSet = new GenericSet2();
+        while(this.size() > 0)
+        {
+            T temp = this.removeAny();
+            set.insert(temp);
+            tempSet.insert(temp);
+        }
+        while(tempSet.size() > 0)
+        {
+            this.insert((T)tempSet.removeAny());
+        }
     }
     @Override
     public final boolean equals(Object obj) {
-/* insert code here */
-    return true;
+        boolean equalsFlag = false;
+        if (obj != null && obj instanceof GenericSet) {
+            GenericSet set = (GenericSet) obj;
+            if (this.size() == set.size()) {
+                equalsFlag = true;
+
+                Object[] tmpArr = new Object[this.size()];
+
+                int i = 0;
+                while (this.size() > 0) {
+                    tmpArr[i] = this.removeAny();
+                    if (!set.contains(tmpArr[i])) {
+                        equalsFlag = false;
+                    }
+                    ++i;
+                }
+
+                for (i = 0; i < tmpArr.length; ++i) {
+                    this.insert((T)tmpArr[i]);
+                }
+            }
+        }
+        return equalsFlag;
     }
     /**
      * This method returns a string representation of the set,
@@ -64,6 +142,23 @@ public abstract class AbstractGSE<T> implements GenericSetExtended<T> {
     @Override
     public final String toString() {
 /* insert code here */
-    return "testing";
+        StringBuilder sb = new StringBuilder();
+        sb.append("{ ");
+
+        Object[] tmpArr = new Object[this.size()];
+        int i = 0;
+        while (this.size() > 0) {
+            tmpArr[i] = this.removeAny();
+            sb.append(String.format("\"%s\"", tmpArr[i]));
+            if (this.size() > 0) {
+                sb.append(", ");
+            }
+            ++i;
+        }
+        for(i = 0; i < tmpArr.length; ++i) {
+            this.insert((T)tmpArr[i]);
+        }
+        sb.append(" }");
+        return sb.toString();
     }
 }
