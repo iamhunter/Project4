1f70421eee8e86164f8950e4c8fc6519accd6831
iams3cond <andr3whunter@gmail.com>
2016-11-14 14:37:32 -0500

    Worksish. Not sorted

--- src/edu/clemson/cpsc2150/project4/Main.java
+++ src/edu/clemson/cpsc2150/project4/Main.java
@@ -10,6 +10,8 @@ import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
@@ -37,21 +39,21 @@ public class Main {
             System.exit(1);
         }
 
+        // Read all of the lines from the file.
+        String[] lines = new String[100];
+        try {
+            Files.readAllLines(path, StandardCharsets.US_ASCII).toArray(lines);
+
+        } catch (IOException e) {
+            System.err.println("Coud not read all lines from the file at the given path.");
+            System.err.println("Unrecoverable. Program terminating.");
+            System.exit(1);
+        }
+
         boolean testBool = false;
 
         if (testBool)
         {
-            // Read all of the lines from the file.
-            String[] lines = new String[100];
-            try {
-                Files.readAllLines(path, StandardCharsets.US_ASCII).toArray(lines);
-
-            } catch (IOException e) {
-                System.err.println("Coud not read all lines from the file at the given path.");
-                System.err.println("Unrecoverable. Program terminating.");
-                System.exit(1);
-            }
-
             // Parse the words out of line one and line two.
             GenericSetExtended firstLine = new GenericSet1(100);
             GenericSetExtended secondLine = new GenericSet2();
@@ -106,6 +108,62 @@ public class Main {
         }
         else
         {
+            // Parse the words out of line one and line two.
+            HashSet firstLine = new HashSet();
+            TreeSet secondLine = new TreeSet();
+
+            int count = 0;
+            for (int i = 0; lines[i] != null; i++) {
+                count++;
+            }
+            if (count % 2 != 0) {
+                count--;
+            }
+
+            for (int i = 0; i < count; i = i + 2) {
+                for (Object word : lines[i].split(" "))
+                    if (!firstLine.contains(word))
+                        firstLine.add(word);
+                for (Object word : lines[i + 1].split(" "))
+                    if (!secondLine.contains(word))
+                        secondLine.add(word);
+
+
+
+                // Run the union, intersection, and difference operators.
+                GenericSetExtended union = new GenericSet2(), intersection = new GenericSet2(), difference = new GenericSet2(), secondLine2 = new GenericSet2();
+
+                Iterator firstIter = firstLine.iterator();
+                while(firstIter.hasNext())
+                {
+                    Object temp = firstIter.next();
+                    union.insert(temp);
+                    intersection.insert(temp);
+                    difference.insert(temp);
+                }
+
+                Iterator secondIter = secondLine.iterator();
+                while(secondIter.hasNext())
+                {
+                    secondLine2.insert(secondIter.next());
+                }
+
+                union.union(secondLine2);
+                intersection.intersect(secondLine2);
+                difference.difference(secondLine2);
+
+                // Print all of the sets, including the state of the initial sets.
+                System.out.println("First line: " + firstLine);
+                System.out.println("Second line: " + secondLine);
+
+                System.out.println();
+
+                System.out.println("Union: " + union);
+                System.out.println("Intersection: " + intersection);
+                System.out.println("Difference: " + difference);
+
+                System.out.println();
+            }
 
         }
     }
